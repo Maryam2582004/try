e28d96ea988091f1ff63f66eb8d94492ad6ab3f3
Maryam2582004 <153773219+Maryam2582004@users.noreply.github.com>
2025-09-26 18:50:45 +0300

    Update pyt.py

--- pyt.py
+++ pyt.py
@@ -2,7 +2,7 @@ import streamlit as st
 import streamlit.components.v1 as components
 
 # نقرأ محتوى ملف HTML ونمرره للـ components
-with open("index.html", "r", encoding="utf-8") as f:
+with open("genoscene.html", "r", encoding="utf-8") as f:
     html_code = f.read()
 
 components.html(html_code, height=500, scrolling=True)
